9484146e876426f7654a66196127343efd50605d
nilakanta123 <nilakanta.kakching@gmail.com>
2017-06-01 21:24:46 +0530

    Update views, models, requirements

--- news/models.py
+++ news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Feed(models.Model):
+    f_name = models.CharField(max_length=200)
+    f_url = models.URLField()
+    f_region = models.CharField(max_length=200)
+    f_notes = models.TextField()
+
+    def __unicode__(self):
+        return self.f_name
+
+class Article(models.Model):
+    feed = models.ForeignKey(Feed)
+    a_title = models.CharField(max_length=200)
+    a_url = models.URLField()
+    a_description = models.TextField()
+    a_pub_date = models.DateTimeField()
+    a_cluster = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.a_title
+
--- news/views.py
+++ news/views.py
@@ -1,6 +1,39 @@
+import feedparser
+import datetime
+from .models import Article, Feed
 from django.shortcuts import render
 from django.views.generic import View 
 
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
 		return render(request, "index.html", {'range': range(3)})
+
+def cron(request):
+    feed_list = Feed.objects.all()
+
+    for feed in feed_list:
+    	try:
+    		feed_data = feedparser.parse(feed.f_url)
+    	except:
+    		print ("Feed Parsing Error")
+    		pass
+    	for entry in feed_data.entries:
+	        existing_entry = Article.objects.filter(a_title=entry.title)
+	        if len(existing_entry) == 0:
+	            article = Article()
+	            article.a_title = entry.title
+	            article.a_url = entry.link
+	            article.a_description = entry.description
+	            d = datetime.datetime(*(entry.published_parsed[0:6]))
+	            date_string = d.strftime('%Y-%m-%d %H:%M:%S')
+	            article.a_pub_date = date_string
+	            article.feed = feed
+	            article.save()
+
+    return render(request, 'cron.html')
+
+
+
+
+
+
